114ec57ad828d92d5534f1212a7a5348f4440a40
Christoph Koschel <christoph.koschel@gmail.com>
2022-12-05 20:53:21 +0100

    Fix atop not defined

--- assets/wrapper/res.ts
+++ assets/wrapper/res.ts
@@ -1,12 +1,26 @@
+import {atob} from "buffer";
+
 type ResourcesID = {
     id: number;
 }
 
 const data: Map<number, string> = new Map<number, string>();
 
+function decode(data: string): string {
+    if (typeof atob == "undefined") {
+        if (typeof Buffer == "undefined") {
+            throw "Cannot decode resources no atop and no Buffer class declared";
+        } else {
+            return Buffer.from(data, "base64").toString("utf-8");
+        }
+    } else {
+        return atob(data);
+    }
+}
+
 export function load_resources(id: ResourcesID): string {
     if (data.has(id.id)) {
-        return atob(<string>data.get(id.id));
+        return decode(<string>data.get(id.id));
     }
     throw "Resources not declared";
 }
